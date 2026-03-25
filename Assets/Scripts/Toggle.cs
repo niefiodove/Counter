e0d42bc1a45f445a34e6aa1675e0ac79ad98bf3e
niefiodove <niefiodov.e@gmail.com>
2026-03-25 14:33:57 +0300

    Sharing of Responsibility

--- Assets/Scripts/Toggle.cs
+++ Assets/Scripts/Toggle.cs
@@ -1,24 +1,9 @@
 using System;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Toggle : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _text;
-
-    private string _titleTextStart = "Нажми, чтобы начать отсчёт.";
-    private string _titleTextStop = "Нажми, чтобы остоновить отсчёт.";
-    private Color _startColor = Color.green;
-    private Color _stopColor = Color.red;
-
     public event Action ButtonClicked;
-
-    private void Start()
-    {
-        ChangeText(_titleTextStart, _startColor);
-    }
 
     private void Update()
     {
@@ -30,16 +15,5 @@ public class Toggle : MonoBehaviour
     private void OnButtonClick()
     {
         ButtonClicked?.Invoke();
-
-        if(_text.text == _titleTextStart)
-            ChangeText(_titleTextStop, _stopColor);
-        else 
-            ChangeText(_titleTextStart, _startColor);
-    }
-
-    private void ChangeText(string text, Color color)
-    {
-        _text.color = color;
-        _text.text = text;
     }
 }
---
+++ Assets/Scripts/Visualizer.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+
+public class Visualizer : MonoBehaviour
+{
+    [SerializeField] private Toggle _toggle;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private string _titleTextStart = "Нажми, чтобы начать отсчёт.";
+    private string _titleTextStop = "Нажми, чтобы остоновить отсчёт.";
+    private Color _startColor = Color.green;
+    private Color _stopColor = Color.red;
+
+    private void OnEnable()
+    {
+        _toggle.ButtonClicked += OnButtonClick;
+    }
+
+    private void OnDisable()
+    {
+        _toggle.ButtonClicked -= OnButtonClick;
+    }
+
+    private void Start()
+    {
+        ChangeText(_titleTextStart, _startColor);
+    }
+
+    private void ChangeText(string text, Color color)
+    {
+        _text.color = color;
+        _text.text = text;
+    }
+
+    private void OnButtonClick()
+    {
+        if (_text.text == _titleTextStart)
+            ChangeText(_titleTextStop, _stopColor);
+        else
+            ChangeText(_titleTextStart, _startColor);
+    }
+}

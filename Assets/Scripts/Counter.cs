ad58fdb01b2baa8fd055acabe5d3eebdd11becd6
niefiodove <niefiodov.e@gmail.com>
2026-03-25 14:19:54 +0300

    unnecessary checking has been removed

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -8,7 +8,6 @@ public class Counter : MonoBehaviour
     [SerializeField] private Toggle _toggle;
 
     private Coroutine _countingCoroutine;
-    private bool _isActive = false;
     private float _currentValue = 0f;
     private float _stepCount = 0.5f;
     private float _stepDelay = 1f;
@@ -29,19 +28,14 @@ public class Counter : MonoBehaviour
     }
     private void OnButtonClick()
     {
-        if( _isActive )
+        if (_countingCoroutine != null)
         {
-            if(_countingCoroutine != null)
-            {
-                StopCoroutine(_countingCoroutine);
-                _countingCoroutine = null;
-            }
-                _isActive = false;
+            StopCoroutine(_countingCoroutine);
+            _countingCoroutine = null;
         }
         else
         {
             _countingCoroutine = StartCoroutine(Counting());
-            _isActive = true;
         }
     }
 
